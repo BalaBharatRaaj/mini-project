68fb05dddf1da140d646d3dcb32ddcf1462903fd
Naresh2002k <66559080+Naresh2002k@users.noreply.github.com>
2020-07-24 12:58:09 +0530

    Update main.c
    Threading and Changes in the program itself to accommodate it.

--- Dam warning system/main.c
+++ Dam warning system/main.c
@@ -2,8 +2,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h> 
+#include <pthread.h> 
 #define g 9.8
 
+pthread_mutex_t loc;
 //Creating a structure variable to store several dam details.
 
 typedef struct flood_system
@@ -25,8 +30,13 @@ void protocol_1();
 void protocol_2();
 double calculate(m dam, double shut_height);
 int decision(m dam, double flow_out);
-void cond(int check, int flag, int y);
+void cond(int check, int flag, int y, int i);
 void file_print_struct(m dam, int n);
+void write_to_file(char dam_info[][10], int i);
+void* create_sample(void* args);
+void split_string(char str[100], int ent);
+double cube_root(double number);
+
 
 //Creating a function for the protocols to be followed during a signal red emergency
 
@@ -80,13 +90,21 @@ double square_root(double number)
 	// Then, replace values (256 / 2 * 128) / 2 = 65
 	//(on second iteration 34.46923076923077)
 	//and so on
-	sqt = (number / 2*temp)/2;      
+	sqt = (number /temp + temp)/2;      
     }
     return sqt;
 }
-//Function to check whether the dam and its surrounding area is safe from flooding
+//Function to check whether safe from flooding
 
-void cond(int check, int flag, int y)
+double cube_root(double num) 
+{ 
+    double ans; 
+    ans = pow(num, 1.0/3.0);
+    ans++; 
+    return ans; 
+}
+
+void cond(int check, int flag, int y, int i)
 {
     int chfl = check + flag;
     if((chfl) == 2)
@@ -94,14 +112,14 @@ void cond(int check, int flag, int y)
             FILE *file;
             file = fopen("records.txt", "a");
 
-            printf("\nAt time t= %d hours \nSignal = RED", y);
+            printf("\nAt time t= %d minutes \nSignal = RED", i);
             printf("\n\nAlert!!The dam and its surrounding areas are on the verge of flooding."
-                    "\nSo kindly evacuate the people from the surrounding areas and move them them to higher grounds until futher adviced.");
+                    "\nSo kindly evacuate the people from the surrounding areas and move them them to higher grounds until futher adviced.\n");
             protocol_1();
 
-            fprintf(file, "\nAt time t=%d hours \nSignal=RED", y);
+            fprintf(file, "\nAt time t = %d minutes \nSignal = RED", i);
             fprintf(file, "\nAlert!!The dam and its surrounding areas are on the verge of flooding."
-                    "\nSo kindly evacuate the people from the surrounding areas and move them them to higher grounds until futher adviced.");
+                    "\nSo kindly evacuate the people from the surrounding areas and move them them to higher grounds until futher adviced.\n");
 
             fclose(file);
     }
@@ -111,14 +129,14 @@ void cond(int check, int flag, int y)
         FILE *file;
         file = fopen("records.txt", "a");
 
-        printf("\nAt time t= %d hours \nSignal = ORANGE", y);
+        printf("\nAt time t= %d minutes \nSignal = ORANGE", i);
         printf("\n\nThe dam and its surrounding areas are at high risk of flooding."
-                "\nSo kindly take the necessary steps required");
+                "\nSo kindly take the necessary steps required\n");
         protocol_2();
 
-        fprintf(file, "\nAt time t = %d hours \nSignal = ORANGE", y);
+        fprintf(file, "\nAt time t = %d minutes \nSignal = ORANGE", i);
         fprintf(file, "\n\nThe dam and its surrounding areas are at high risk of flooding."
-                "\nSo kindly take the necessary steps required");
+                "\nSo kindly take the necessary steps required\n");
 
         fclose(file);
     }
@@ -128,19 +146,19 @@ void cond(int check, int flag, int y)
         FILE *file;
         file = fopen("records.txt", "a");
 
-        printf("\nAt time t= %d hours \nSignal = GREEN", y);
+        printf("\nAt time t = %d minutes \nSignal = GREEN", i);
         printf("\n\nThe dam and its surrounding area is safe from flooding now and no need to evacuate the people "
                 "from neighbouring areas."
-                "\nNo need to panic.");
+                "\nNo need to panic.\n");
 
-        fprintf(file, "\nAt time t = %d hours \nSignal = GREEN", y);
+        fprintf(file, "\nAt time t = %d minutes \nSignal = GREEN", i);
         fprintf(file, "\n\nThe dam and its surrounding area is safe from flooding now and no need to evacuate the people "
                 "from neighbouring areas"
-                "\nNo need to panic");
+                "\nNo need to panic\n");
 
         fclose(file);
 
-        exit(0);
+        //exit(0);
     }
 }
 
@@ -150,6 +168,7 @@ int capacity(m dam)
 {
     double cap = dam.lat_area*dam.water_level;
 
+    printf("cap is %lf : dam threshold is %lf \n ",cap, dam.threshold);
     if(cap>dam.threshold)
     {
         return 1;
@@ -166,6 +185,7 @@ double calculate(m dam, double shut_height)
 {
     double velocity = square_root((2*g*shut_height));
     double area = dam.width * shut_height;
+    printf("test_velocity: %lf, velocity : %lf, area : %lf, calculate : %lf\n",2*g*shut_height, velocity, area, area*velocity);
     return (area * velocity);
 }
 
@@ -174,6 +194,8 @@ double calculate(m dam, double shut_height)
 int decision(m dam, double flow_out)
 {
     double dh = (dam.flow_in - flow_out)/dam.lat_area;
+    
+    printf("flow_out is %lf, dh is %lf, dam.init_height is %lf\n",flow_out, dh, dam.init_height);
 
     if(dh > dam.init_height)
     {
@@ -186,6 +208,80 @@ int decision(m dam, double flow_out)
     }
 }
 
+//Function to generate a sample record for a dam
+
+void* create_sample(void* args)
+{
+    pthread_mutex_lock(&loc);
+    FILE* fptr;
+    double threshold = 443230000;
+    double lat_area = 19623.462;
+    double flow_in = 1028400;
+    double width = 365.7;
+    double water_level = 20000;
+    double init_height = 53.6;
+    fptr=fopen("sample.txt","w");
+    fprintf(fptr, "Name,Threshold_Capacity,Lateral_Area,Inward_Flow,Width,Water_Level,Initial_Height\n");
+    for(int t=0;t<1440;t++)
+    {
+        fprintf(fptr, "Periyar,%lf,%lf,%lf,%lf,%lf,%lf\n",threshold, lat_area, flow_in, width, water_level, init_height);
+        water_level += 10;
+        flow_in += 250;
+    }
+    fclose(fptr);
+    pthread_mutex_unlock(&loc);
+}
+
+//Function to find shutter height
+
+double find_shut_height(double flow_in, double init_height, double width, double lat_area)
+{
+    double shut_height, value;
+    value = square_root(flow_in - lat_area*init_height);
+    shut_height = value/(width*2*g);
+    shut_height = cube_root(shut_height) + 1;
+    return shut_height;
+}
+//Function to read from a given record
+
+void split_string(char str[100], int ent)
+{
+    char splitString[7][10]; //Only 7 columns
+    int i,j,cnt;
+    j=0; cnt=0;
+    for(i=0;i<=(strlen(str));i++)
+    {
+        // if comma or NULL found, assign NULL into splitStrings[cnt]
+        if(str[i]==','||str[i]=='\0')
+        {
+            splitString[cnt][j]='\0';
+            cnt++;  //for next value
+            j=0;    //for next value, init index to 0
+        }
+        else
+        {
+            splitString[cnt][j]=str[i];
+            j++;
+        }
+    }
+    write_to_file(splitString, ent);
+}
+
+//Function to assign values from split_string()
+m dam[1440];
+void write_to_file(char dam_info[][10], int i)
+{
+    //atof is used to convert the numerical string to type double, should
+    //there be a limit on the decimal places?
+    strcpy(dam[i].name,dam_info[0]);
+    dam[i].threshold = atof(dam_info[1]);
+    dam[i].lat_area = atof(dam_info[2]);
+    dam[i].flow_in = atof(dam_info[3]);
+    dam[i].width = atof(dam_info[4]);
+    dam[i].water_level = atof(dam_info[5]);
+    dam[i].init_height = atof(dam_info[6]);
+    //printf("%s,%lf,%lf,%lf,%lf,%lf,%lf\n", dam[i].name, dam[i].threshold, dam[i].lat_area, dam[i].flow_in, dam[i].width, dam[i].water_level, dam[i].init_height);
+}    
 //Function to store the entered details of the dam in a file for future reference
 
 void file_print_struct(m dam, int n)
@@ -217,24 +313,35 @@ void file_print_struct(m dam, int n)
 
 int main()
 {
-    FILE *ftr;
-
+    pthread_t trd;
+    pthread_create(&trd, NULL, &create_sample, NULL);
+    pthread_join(trd, NULL);
+    FILE *ftr1,*ftr2;
+    pthread_mutex_lock(&loc);
+    //Opening the file called 'sample'
+    ftr1 = fopen("sample.txt","r");
     //Creating a file called 'records'
-    ftr=fopen("records.txt","w");
+    ftr2 = fopen("records.txt","w");
 
     //Closing the file
-    fclose(ftr);
-
+    fclose(ftr2);
+    
     printf("\t\t\t\t\tEARLY WARNING FLOOD SYSTEM\n\n");
-    int n;
-    int num;
-    printf("\nEnter the number of dams in the system that are being monitored :");
-
-    scanf("%d", &n);
-    m dam[n];
-    for(int i=0; i<n; i++)
+    
+    char line[256];
+    int i = 0, test=0;                                //Dam Entry Number
+    while (fgets(line, sizeof(line), ftr1))
     {
-        printf("\nEnter the name of the dam :");
+        //We don't want the first row as it consists of the column names.
+        if(test==0)
+        {
+            test++;
+            continue;                                   
+        }
+        //printf("%s", line); 
+        split_string(line,i);
+        
+        /* printf("\nEnter the name of the dam :");
         scanf("%s", dam[i].name);
 
         printf("\nEnter the threshold capacity of the dam in SI units :");
@@ -253,10 +360,11 @@ int main()
         scanf("%lf", &dam[i].init_height);
 
         printf("\nEnter the water level in the dam in SI units :");
-        scanf("%lf", &dam[i].water_level);
+        scanf("%lf", &dam[i].water_level); 
 
         //Saving the details of the dam into a file
-        file_print_struct(dam[i], n);
+        
+        //file_print_struct(dam[i], n);*/
         double flow_out = 0.00, shut_height = 0.00;
         int check, flag, y = 0;
 
@@ -264,65 +372,54 @@ int main()
 
         check = capacity(dam[i]);
         flag = decision(dam[i], flow_out);
-        cond(check, flag, y);
+        cond(check, flag, y, i);
         y = y + 1;
-
-	while(2)
+        sleep(1);
+	    while(2)
         {
             if(check==0 && flag==0 && y!=0)                     //Checking if signal is GREEN
                 {
-                    FILE *file;
+                    /*FILE *file;
 
                     file = fopen("records.txt", "a");           //Opening the file called 'records'
 
-                    printf("\nAt time t = %d hours \nSignal = GREEN", y);
+                    printf("\nAt time t = %d minutes \nSignal = GREEN", i);
                     printf("\nThe dam and its surrounding area is safe from flooding now and no need to evacuate the people "
 			               "from neighbouring areas."
-			               "\nAnd no need to panic.");
-                    fprintf(file, "\nAt time t=%d hours signal=GREEN", y);
+			               "\nAnd no need to panic.\n");
+                    fprintf(file, "\nAt time t = %d minutes signal = GREEN", i);
                     fprintf(file, "\nThe dam and its surrounding area is safe from flooding now and no need to evacuate the people "
 			                "from neighbouring areas. "
-			                "And no need to panic");
+			                "And no need to panic\n");
 
-                    fclose(file);                               //Closing the file
-
-                    exit(0);
+                    fclose(file);                               //Closing the file*/
                 }
 
             else
                 {
-                           ftr = fopen("records.txt","a");                              //Opening the file called 'records'
+                           ftr2 = fopen("records.txt","a");                              //Opening the file called 'records'
 
                            printf("\n\nRise the shutter further to raise the outward flow in the dam ");
-                           fprintf(ftr,"\n\nRise the shutter further to raise the outward flow in the dam ");
+                           fprintf(ftr2,"\n\nRise the shutter further to raise the outward flow in the dam ");
 
-                           printf("\n\n1.Continue the program.");                       //Menu
-                           printf("\n2.Exit the program immediately.");
-                           printf("\n\nEnter any one of the above values: ");
+                           //printf("\n\n1.Continue the program.");                       //Menu
+                           //printf("\n2.Exit the program immediately.");
+                           //printf("\n\nEnter any one of the above values: ");
 
-                           scanf("%d",&num);
-
-                           if(num == 2)
-                           {
-                               fprintf(ftr,"\n\nProgram finished.");
-                               fclose(ftr);
-                               exit(0);                                                 //Exiting the program
-                           }
-                           else
-                           {
-                               printf("\n\nEnter the amount by which the shutter in the dam is to be risen: ");
-                               scanf("%lf",&shut_height);                               //Input for shutter height
-
-
-                               fprintf(ftr,"\n\nEnter the amount by which the shutter in the dam is to be risen: %lf",shut_height);
-
-
-                               flow_out = calculate(dam[i], shut_height);               //Calculating the outward flow
-                               printf("\nEnter the current water level in the dam in SI units:");
-                               scanf("%lf",&dam[i].water_level);                        //Input for water level
+                           //scanf("%d",&num);
+                            if(1)
+                            {
+                               //flow_out = calculate(dam[i], shut_height);               //Calculating the outward flow
+                               printf("\nThe current water level in the dam is: %lf\n\n",dam[i].water_level);
                                printf("\n\n");
 
-                               fprintf(ftr,"\nEnter the current water level in the dam in SI units: %lf\n\n",dam[i].water_level);
+                               fprintf(ftr2,"\nThe current water level in the dam is: %lf\n\n",dam[i].water_level);
+                               
+                               shut_height = find_shut_height(dam[i].flow_in, dam[i].init_height, dam[i].width, dam[i].lat_area);
+                               printf("\n\nThe amount by which the shutter in the dam has to be risen is : %lf",shut_height);
+
+
+                               fprintf(ftr2,"\n\nThe amount by which the shutter in the dam has to be risen is : %lf",shut_height);
 
                                //Checking the capacity
                                check = capacity(dam[i]);
@@ -330,16 +427,19 @@ int main()
                                //Getting the decision required
                                flag = decision(dam[i], flow_out);
 
-                               cond(check, flag, y);
+                               cond(check, flag, y, i);
                                y = y + 1;
 
-                               fclose(ftr);                             //Closing the file
+                               fclose(ftr2);                             //Closing the file
                             }
 
                     }
-
-           }
+            break;
+           } 
+        i++;   
     }
-
+    pthread_mutex_unlock(&loc);
+    pthread_mutex_destroy(&loc);
+    fclose(ftr1);
     return 0;
 }
